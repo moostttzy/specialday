fed221827ab60e60c9602b09ce95c05644760f76
moostttzy <ahmdmsthf3@gmail.com>
2024-11-23 01:09:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Fungsi untuk menampilkan konten utama
+function showMainContent() {
+    // Hilangkan landing page
+    document.getElementById("landing-page").classList.add("hidden");
+    // Tampilkan halaman utama
+    document.getElementById("main-content").classList.remove("hidden");
+}
